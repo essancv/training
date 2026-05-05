7c6e85b2ba401304c15c9f4e0f805c4890d3ec14
Andres Carrera Vallejo <0012708@viewnext.com>
2026-05-05 16:38:34 +0200

    prueba

---
+++ app/test_prueba.py
@@ -0,0 +1,13 @@
+from prueba import mi_casa
+
+def test_mi_casa():
+    assert mi_casa(1) == 2
+    assert mi_casa(2) == 3
+    assert mi_casa(3) == 4
+    assert mi_casa(4) == 5
+    assert mi_casa(5) == 6
+    assert mi_casa(6) == 7
+    assert mi_casa(7) == 8
+    assert mi_casa(8) == 9
+    assert mi_casa(9) == 10
+    assert mi_casa(10) == 11
